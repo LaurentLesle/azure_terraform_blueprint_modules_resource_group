6c2e7dec5a08844ad996c54d071bae1836de7a99
Laurent <laurent.lesle@microsoft.com>
2019-05-09 10:34:53 +0800

    Removing the - between prefix and name

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ locals {
 resource "azurerm_resource_group" "rg" {
     count                   = "${length(local.rg_list)}"
 
-    name                    = "${var.prefix}-${var.resource_groups[element(local.rg_list, count.index)]}"
+    name                    = "${var.prefix}${var.resource_groups[element(local.rg_list, count.index)]}"
     location                = "${var.location}"
 }
